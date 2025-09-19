a6657a5e41cbc9e07dbae9749ccf77c56cd9c9e0
FlanVessel <122923210+FlanVessel@users.noreply.github.com>
2025-09-19 16:39:42 -0600

    Tenemos UI_Manager

---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using Dino.UtilityTools.Singleton;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class UI_Manager : Singleton<UI_Manager>
+{
+    [SerializeField] private List<UI_Window> uiWindows = new List<UI_Window>();
+
+    public void ShowUI(string windowUI)
+    {
+        foreach (var window in uiWindows)
+        {
+            if (window.WindowID == windowUI)
+            {
+                window.Show();
+                return;
+            }
+        }
+        Debug.LogWarning($"UI Window with name {windowUI} not found.");
+    }
+
+    public void HideUI(string windowUI)
+    {
+        foreach (var window in uiWindows)
+        {
+            if (window.WindowID == windowUI)
+            {
+                window.Hide();
+                return;
+            }
+        }
+        Debug.LogWarning($"UI Window with name {windowUI} not found.");
+    }
+
+    public void HideAllUI()
+    {
+        foreach (var window in uiWindows)
+        {
+            window.Hide();
+        }
+    }
+
+    public UI_Window GetUIWindow(string windowUI)
+    {
+        foreach (var window in uiWindows)
+        {
+            if (window.WindowID == windowUI)
+            {
+                return window;
+            }
+        }
+        Debug.LogWarning($"UI Window with name {windowUI} not found.");
+        return null;
+    }
+
+    #region Editor
+    [Button]
+    private void GetAllUIWindows()
+    {
+        uiWindows.Clear();
+        UI_Window[] windows = FindObjectsByType<UI_Window>(FindObjectsSortMode.InstanceID);
+        uiWindows.AddRange(windows);
+    }
+
+    #endregion
+
+}
+
+public static class WindowsIDs
+{
+    public static string Popup = "PopupUI";
+
+}
