ca46d08480c989b73f5fea8261b298d5386e36b0
FlanVessel <122923210+FlanVessel@users.noreply.github.com>
2025-09-19 20:49:57 -0600

    Cambios y tener que entregar un UI bien hecho el viernes?

--- Assets/Scripts/UI_Window.cs
+++ Assets/Scripts/UI_Window.cs
@@ -15,6 +15,8 @@ public class UI_Window : MonoBehaviour
     [SerializeField] public Ease easeHide = Ease.Flash;
     [SerializeField] public Ease easeShow = Ease.InBack;
 
+    public bool IsShowing { get; private set; } = false;
+
     public string WindowID => windowID;
 
     public void Start()
@@ -32,16 +34,25 @@ public class UI_Window : MonoBehaviour
     {
         //windowCanvas.gameObject.SetActive(true);
 
+        //Si la ventana esta ahi, no haces nada
+        if (IsShowing) return;
+
+        //Activamos el objeto
+        windowCanvas.gameObject.SetActive(true);
+
         if (instant)
         {
 
+            //Mostrar la ventana inmediatamente
             windowCanvasGroup.transform.DOScale(Vector3.one, 0f);
 
         }
         else
         {
 
+            //Mostrar la ventana con animation time
             windowCanvasGroup.transform.DOScale(Vector3.one, animationTime).SetEase(easeShow);
+            IsShowing = true;
 
         }
     }
@@ -54,14 +65,22 @@ public class UI_Window : MonoBehaviour
         if (instant)
         {
 
+            //Ocultar la ventana inmediatamente
             windowCanvasGroup.transform.DOScale(Vector3.zero, 0f);
 
         }
         else
         {
 
-            windowCanvasGroup.transform.DOScale(Vector3.zero, animationTime).SetEase(easeHide);
+            //Ocultar la ventana con animation time
+            windowCanvasGroup.transform.DOScale(Vector3.zero, animationTime).SetEase(easeHide).OnComplete(DisableCanvas);
 
         }
+    }
+
+    private void DisableCanvas()
+    {
+        windowCanvas.gameObject.SetActive(false);
+        IsShowing = false;
     }
 }
