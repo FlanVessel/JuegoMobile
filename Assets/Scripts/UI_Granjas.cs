a75a81f1c59d964eb6ded7d5331a7584f05ac17a
FlanVessel <122923210+FlanVessel@users.noreply.github.com>
2025-10-09 23:26:45 -0600

    Modificaciones

---
+++ Assets/Scripts/UI_Granjas.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class UI_Granjas : UI_Window
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
