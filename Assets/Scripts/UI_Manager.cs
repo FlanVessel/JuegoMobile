bb27443204bdb53b8268ddbd374e588353102905
FlanVessel <122923210+FlanVessel@users.noreply.github.com>
2025-10-10 13:53:05 -0600

    Paneles Listos!!!

--- Assets/Scripts/UI_Manager.cs
+++ Assets/Scripts/UI_Manager.cs
@@ -71,5 +71,14 @@ public static class WindowsIDs
 {
     public static string Popup = "PopupUI";
     public static string UI_Inventory = "InventoryUI";
+    public static string UI_Principal = "PrincipalMenu";
+    public static string UI_Settings = "Settings";
+    public static string UI_Profile = "Profile";
+    public static string UI_Notes = "Notes";
+    public static string UI_Granjas = "Granjas";
+    public static string UI_Tienda = "Tienda";
+    public static string UI_ElegirGranja = "ElegirGranja";
+    public static string UI_ElegirTienda = "ElegirTienda";
+
 
 }
