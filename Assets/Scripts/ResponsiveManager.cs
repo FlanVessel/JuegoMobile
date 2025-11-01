f08f96e6c078690c6c4106eca7c5806b260705ea
FlanVessel <122923210+FlanVessel@users.noreply.github.com>
2025-11-01 14:59:04 -0600

    Modificamos el posible error del Responsive.

--- Assets/Scripts/ResponsiveManager.cs
+++ Assets/Scripts/ResponsiveManager.cs
@@ -2,44 +2,59 @@ using System;
 using Dino.UtilityTools.Singleton;
 using UnityEngine;
 using UnityEngine.Events;
-
-public enum DeviceType
-{
-    Mobile,
-    Tablet,
-}
+using System.Collections.Generic;
 
 public class ResponsiveManager : Singleton<ResponsiveManager>
 {
-    private Vector2 _lastScreensize;
+    #region private Fields
+    private Vector2 _lastScreenSize;
+    #endregion
 
+    #region public Properties
+    public ScreenOrientation CurrentOrientation => GetScreenOrientation();
     public DeviceType CurrentDeviceType { get => GetDeviceTypeByResolution(Screen.width, Screen.height); }
     public bool IsPortrait() => Screen.width < Screen.height;
-
+    public bool IsLandscape() => Screen.width >= Screen.height;
+    public Vector2 CurrentScreenSize => new Vector2(Screen.width, Screen.height);
     public UnityEvent OnScreenSizeChanged { get; private set; } = new UnityEvent();
 
-    private void Onable()
+    #endregion
+
+    #region Unity Methods
+    private void OnEnable()
     {
-        _lastScreensize = new Vector2(Screen.width, Screen.height);
+        _lastScreenSize = new Vector2(Screen.width, Screen.height);
         Application.onBeforeRender += CheckScreenSizeChange;
     }
-
     private void OnDisable()
     {
         Application.onBeforeRender -= CheckScreenSizeChange;
     }
 
+    private void Start()
+    {
+        Debug.Log(CurrentScreenSize);
+        Debug.Log(CurrentOrientation);
+        Debug.Log(CurrentDeviceType);
+    }
+    #endregion
+
+    #region Private Methods
     private void CheckScreenSizeChange()
     {
         Vector2 currentScreenSize = new Vector2(Screen.width, Screen.height);
-        if (_lastScreensize != currentScreenSize)
+        if (_lastScreenSize != currentScreenSize)
         {
-            _lastScreensize = currentScreenSize;
+            _lastScreenSize = currentScreenSize;
             OnScreenSizeChanged?.Invoke();
             Debug.Log($"Screen size changed: {currentScreenSize.x}x{currentScreenSize.y} Orientation: {(IsPortrait() ? "Portrait" : "Landscape")}");
+            Debug.Log($"Device type: {CurrentDeviceType}");
         }
     }
-
+    private ScreenOrientation GetScreenOrientation()
+    {
+        return IsPortrait() ? ScreenOrientation.Portrait : ScreenOrientation.Landscape;
+    }
     private DeviceType GetDeviceTypeByResolution(int width, int height)
     {
         float aspectRatio = (float)Math.Max(width, height) / Math.Min(width, height);
@@ -51,4 +66,16 @@ public class ResponsiveManager : Singleton<ResponsiveManager>
         else
             return DeviceType.Mobile;
     }
+    #endregion
+}
+
+public enum ScreenOrientation
+{
+    Portrait,
+    Landscape
+}
+public enum DeviceType
+{
+    Mobile,
+    Tablet,
 }
